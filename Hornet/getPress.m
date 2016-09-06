56d9e674ad5dfb77153654ff9d8268758d99b420
DK5 <d.karasikov@gmail.com>
2016-09-06 09:31:50 +0300

    From Rack

--- Hornet/getPress.m
+++ Hornet/getPress.m
@@ -1,3 +1,13 @@
+obj1 = instrfind('Type', 'serial', 'Port', 'COM1', 'Tag', '');
+
+% Create the serial port object if it does not exist
+% otherwise use the object that was found.
+if isempty(obj1)
+    obj1 = serial('COM1');
+else
+    fclose(obj1);
+    obj1 = obj1(1)
+end
 Hornet = serial('COM3');    % specify com #
 fopen(Hornet);
 
--- Scripts/getDC.m
+++ Scripts/getDC.m
@@ -1,13 +1,13 @@
 function outDC = getDC(N4TH)
     % read DC current
     fprintf(N4TH,'COUPLI,PHASE1,DCONLY'); % set DC coupling
-    fprintf(N4TH,'FAST,ON');	% fast communication mode - on
+%     fprintf(N4TH,'FAST,ON');	% fast communication mode - on
     reading = [];   % prepare empty readings
     while isempty(reading)
         pause(.5);
         reading = query(N4TH,'VRMS,RMS?');  % read
     end
-    fprintf(N4TH,'FAST,OFF');	% fast communication mode - off
+%     fprintf(N4TH,'FAST,OFF');	% fast communication mode - off
     vrms = textscan(reading,'%s','Delimiter', ',',...
                     'CommentStyle','\','headerlines',0);
     vrms = vrms{:};
