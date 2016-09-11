9574bd915026b18367d56f9cbc9edc95c6891a0c
DK5 <d.karasikov@gmail.com>
2016-09-11 13:47:24 +0300

    loop control feedbacks
    Temp & DC current, still not fully functional.

--- Scripts/setDC.m
+++ Scripts/setDC.m
@@ -3,8 +3,8 @@ function [ outDC ] = setDC( iDC , pwr_obj , N4TH )
 %   iDC - desired DC current
 %   pwr_obj  - power supplier object
 %   N4TH- N4TH object
-amp = 1 ; % amplification factor of dI/I
-damp = 0.01;
+amp = 1.5 ; % amplification factor of dI/I
+damp = 0.005;
 
 outputHP(0,pwr_obj);    % turn off
 supVoltage(5,pwr_obj);  % set voltage to 5 Volts
@@ -12,7 +12,7 @@ supCurrent(iDC,pwr_obj);% set current
 outputHP(1,pwr_obj);    % turn on
 
 % read DC current
-pause(5);
+pause(5); dt = 1;
 out2DC = getDC(N4TH);
 
 ind = 1;    
@@ -20,36 +20,36 @@ ind = 1;
 while abs(out2DC/iDC - 1) > 0.002
 %     outputHP(0,pwr_obj);
     out1DC = out2DC;    % save old current
-    pause(0.1);         % pause before another measure
+    pause(1);         % pause before another measure
     out2DC = getDC(N4TH);   % measure current
-    dI = out2DC-out1DC; % calculate difference between two close measurements
-    if out2DC > 0.6*iDC
+    dI = abs(out2DC-out1DC)/dt; % calculate difference between two close measurements
+    if out2DC < 0.8*iDC
         % low dI, low I (out2DC) --> high outDC
-        outDC = iDC*(1+damp/(dI*out2DC));
+        outDC = iDC*(1+damp/(dI*out2DC))
     else
         % I is close
-        outDC = iDC*(1+amp*dI/out2DC);
+        outDC = iDC*(1+amp*dI/out2DC)
     end
     
-    if outDC >= 75 && ind <= 3
+    if outDC >= 15 && ind <= 3
         % if current is too high and its under the 3rd try
         fprintf ('Try %i\n',ind);
         fprintf('Current is too high!!! %2.2f Amps \n ',outDC);
-        fprintf('Cannot reach %2.2fA. Actual current is %2.2fA\n',iDC,outDC);
+        fprintf('Cannot reach %2.2fA. Actual current is %2.2fA\n',outDC,out2DC);
         % turn off power supplier
 %         outDC = 0; supCurrent(outDC,pwr_obj);   % set current
         ind = ind+1; outDC = 0.01*ind;
         
-    elseif outDC >= 75 && ind > 3;
+    elseif outDC >= 15 && ind > 3;
         % if amplitude is too high and its over the 3rd try
         fprintf ('Couldn''t make it :( \n');
         % turn off power supplier and break
-        outDC = 0; supCurrent(outDC,pwr_obj); output(0,pwr_obj); break;
+        outDC = 0; supCurrent(outDC,pwr_obj); outputHP(0,pwr_obj); break;
     end
     
     supCurrent(outDC,pwr_obj);	% set current
 %     outputHP(1,pwr_obj);        % turn output on
-    pause(5);                   % wait for power supply
-    out2DC = getDC(N4TH);        % get DC current
+    pause(3);                   % wait for power supply
+    out2DC = getDC(N4TH)        % get DC current
 end
 end
--- Temperature/setTemp.m
+++ Temperature/setTemp.m
@@ -1,37 +1,43 @@
 function setTemp(spTemp,errorInt,Isrc,Rth,volt_obj,XFR)
 
-figure('Temperature control');  % open fig
+figure('Name','Temperature control');  % open fig
 err = [];   % declare variable
 
 for samp = 1:3
     measTemp = getTemp(volt_obj,Isrc);      % measure 5 temperatures
     err(samp) = (spTemp - measTemp)/spTemp; % calculate 5 initial errors
 end
+
 pHandle = plot(1:length(err),spTemp - err*spTemp,'-ob',[1 length(err)],[spTemp spTemp],'-r');
+% linkdata on;
+hold off;
 
 % PID constants
 % u(t) = Kp*e(t) + Ki*integral({0,t},e(\tau),d\tau) + Kd*(de/dt)
-dt = 0.05;
-Kp = 5; Ki = 3; Kd = 3;
-Power = 5;
+dt = 0.2; cor = 30;
+Kp = 10; Ki = 10; Kd = 10;
+Power = 20;
 
-stable = 0;
+stable = 0; j = 3;
 while ~stable
+    j=j+1
     pause(dt)
     % measure Temperature
     measTemp = getTemp(volt_obj,Isrc);
     err(end+1) = (spTemp - measTemp)/spTemp;
+    plot(1:length(err),spTemp - err*spTemp,'-ob',[1 length(err)],[spTemp spTemp],'-r');
     % refresh plot
-    refreshdata(pHandle,'base'); drawnow;
-
+%     refreshdata(pHandle,'caller'); drawnow;
+    
     intg = trapz(err)*dt;
-    derr = (err(end-1) - err(end))/dt;
+    derr = (err(end) - err(end-1))/dt;
 
-    u = Kp*err(end)+Ki*intg+Kd*derr;
-    xfrPower(u*Power, Rth , XFR );
+    u = Kp*err(end)+Ki*intg+Kd*derr
+    xfrPower((1+u)*Power, Rth , XFR );
 
-    if numel(err) > 10 && sum(abs(err(end-50))> errorInt/spTemp)
+    if numel(err) > cor && ~sum(abs(err(end-cor)) > errorInt/spTemp)
         % if last 10 temperatures were in the interval - stop
+        num2str(spTemp - spTemp*err(end-cor))
         stable = 1;
     end
 end
